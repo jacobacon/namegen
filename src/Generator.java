cad9d1dda93da6dab020765ab767f248c493447c
Jacob Beneski <jacobbeneski@gmail.com>
2015-02-25 16:00:33 -0600

    Basic functionality in progress. Gives a hard coded name.

--- src/Generator.java
+++ src/Generator.java
@@ -1,11 +1,27 @@
-
 public class Generator {
-	public String createFirstName(){
-		String name = "Jacob";
+	public String createFirstName(String gender) {
+		String name;
+		if(gender.equalsIgnoreCase("male")){
+			name = "Jacob";
+		}
+		else
+			if(gender.equalsIgnoreCase("female")){
+				name = "Josephine";
+			}
+			else{
+				name = null;
+			}
 		return name;
 	}
-	
-	
-	
+
+	public String createMiddleName() {
+		String name = "Thomas";
+		return name;
+	}
+
+	public String createLastName() {
+		String name = "Beneski";
+		return name;
+	}
 
 }
--- src/NameGen.java
+++ src/NameGen.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class NameGen {
-	private String fName, mName, lName;
+	private static String fName, mName, lName,gender,fullName;
 	private static Generator gen = new Generator();
+	static Scanner scan = new Scanner(System.in);
 
 
 	public static void main(String[] args) {
 		System.out.println("Starting Name Generator");
-		System.out.println("Creating First Name:");
-		System.out.println(gen.createFirstName());
+		System.out.println("What Gender do you want? (male/female)");
+		gender = scan.nextLine();
+		System.out.println("Choosing First Name...");
+		fName = gen.createFirstName(gender);
+		System.out.println("Choosing Middle Name... ");
+		mName = gen.createMiddleName();
+		System.out.println("Choosing Last Name... ");
+		lName = gen.createLastName();
+		
+		fullName = fName + " " + mName + " " + lName;
+		System.out.println("Your name is: " + fullName);
 		
 
 	}
